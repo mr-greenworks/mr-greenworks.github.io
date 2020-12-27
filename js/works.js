9a1bdeb4b97474d25ba036d328b7db927623a3ca
りーぜんと <len.hirata@gmail.com>
2020-12-27 16:54:44 +0900

    adsfa

--- js/works.js
+++ js/works.js
@@ -1,61 +1,65 @@
-function open_work(category, title, img_cnt) {
+window.onload = async function() {
+    grant_effect();
+    
+    $(window).scroll(function(){
+      grant_effect();
+    });
+  }
+
+function grant_effect() {
+    $('.effect-fade').each(function () {
+        var elemPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+  
+        if (scroll > elemPos - windowHeight) {
+            if ($(this).hasClass('menu-title') || $(this).hasClass('work-title')) {
+                $(this).addClass('effect-scroll09');
+            } else {
+                $(this).addClass('effect-scroll1');
+            }
+        }
+    })
+  }
+
+function open_work(category, title) {
     var html = `
-        <button onclick="location.href = '../html/works.html'">
-            <img src='../img/header.png' class='header-image'>
-        </button>
-
-        <div id="carousel-work" class="carousel slide carousel-fade" data-interval='false'>
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-work" data-slide-to="0" class="active"></li>
+        <div style="height: 60px;"></div>
+        <h4 class='caption effect-fade left' style='text-align: center;'><u>${works[category][title].heading}</u></h4>
+        <div style="height: 30px;"></div>
     `;
 
-    for (var i = 1; i < img_cnt; i++) {
-        html += '<li data-target="#carousel-work" data-slide-to="${i}"></li>';
-    }
-
-    html += `
-        </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item pad active">
-                    <img src='../works/${category}/${title}/0.jpg' class='carousel-image'>
-                </div>
-    `;
-
-    for (var i = 1; i < img_cnt; i++) {
+    for (var i = 0; i < works[category][title].img_cnt; i++) {
         html += `
-                <div class="carousel-item pad">
-                    <img src='../works/${category}/${title}/${i}.jpg' class='carousel-image'>
-                </div>
+            <div class='article-image-cover effect-fade bottom'><img class='article-image' src="../works/${category}/${title}/${i}.jpg"></div>
+            <div class='caption effect-fade left'>
+                <p>${works[category][title].contents[i]}</p>
+            </div>
         `;
     }
-    
-    html += `
-            </div>
-        </div>
-    `;
 
-    document.body.innerHTML = html;
+    document.getElementById('stage').innerHTML = html;
 }
 
 function open_category(category) {
     var html = '';
 
-    works[category].forEach(work => {
+    for (let title in works[category]) {
         html += `
             <div class='d-flex align-items-center work-item'>
-                <img src='../works/${category}/${work.title}/0.jpg' class='work-image effect-fade bottom'>
-                <button class='row align-items-center work-title effect-fade left' onclick='open_work("${category}", "${work.title}", ${work.img_cnt})'>
+                <img src='../works/${category}/${title}/0.jpg' class='work-image effect-fade bottom'>
+                <button class='row align-items-center work-title effect-fade left' onclick='open_work("${category}", "${title}"); grant_effect();'>
                     <div class='col'>
-                        <h3>${work.title}</h3>
-                        <p>${work.year}年施工</p>
+                        <h5>${title}</h5>
+                        <p>${works[category][title].year}年施工</p>
                     </div>
                     <div class='col-3'>
-                        <img src='../img/right_arrow.png' class='icon'>
+                        <img src='../img/right_arrow.jpg' class='icon'>
                     </div>
                 </button>
             </div>
         `;
-    });
+    }
 
     document.getElementById('stage').innerHTML = html;
 }
